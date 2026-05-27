2a78db432671a974d6405507a4668cf18e189da5
Tardigraade <ryannfrr@protonmail.com>
2026-05-27 16:14:35 +0200

    Commit
    comit

--- Test_cam.py
+++ Test_cam.py
@@ -7,6 +7,11 @@ sensor.reset()
 sensor.set_pixformat(sensor.GRAYSCALE)
 sensor.set_framesize(sensor.B64X64) # ésolution carrée  pour l'optical flow
 
+# Pour éviter les artefacts de lumière
+sensor.set_auto_gain(False)
+sensor.set_auto_whitebal(False)
+
+
 old_img = sensor.snapshot()
 clock = time.clock()
 
@@ -16,4 +21,5 @@ while(True):
 
     # 1. Calcul du déplacement en pixels
     displacement = img.find_displacement(old_img)
+    old_img = img.copy() # l'image actuelle devient l'ancienne
     print(clock.fps())
